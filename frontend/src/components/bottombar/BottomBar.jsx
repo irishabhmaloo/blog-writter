812eb7dba6aedb7f7b38d3ee3fba8292d0b1cc1c
Nirdeshi <nirdeshijotangia@gmail.com>
2023-11-03 01:43:10 +0530

    Frontend added

---
+++ frontend/src/components/bottombar/BottomBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './bottombar.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="contact-info">
+        <h2>Contact Us</h2>
+        <p>SVNIT</p>
+        <p>Surat, India</p>
+        <p>Email: rishnir@example.com</p>
+        <p>Phone: xxxxxxxxxx</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
