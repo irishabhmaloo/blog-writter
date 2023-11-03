690e7aa6b8ce3465098adeb3328cf091994a07ec
NirdeshiJotangia <76218812+NirdeshiJotangia@users.noreply.github.com>
2023-11-03 15:13:14 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
 const multer = require("multer");
 const path = require("path");
+const cors = require('cors');
 
 dotenv.config();
 app.use(express.json());
