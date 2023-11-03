61d2479e5ccd79806f4e97efdaaded025b2cdce7
Rishabh Maloo <76218732+irishabhmaloo@users.noreply.github.com>
2023-11-03 14:37:14 +0530

    Update Posts.jsx

--- frontend/src/components/posts/Posts.jsx
+++ frontend/src/components/posts/Posts.jsx
@@ -4,9 +4,13 @@ import "./posts.css";
 export default function Posts({ posts }) {
   return (
     <div className="posts">
-      {posts.length > 0 ? posts.map((p) => (
-        <Post post={p} />
-      )) : <h1>NO</h1>}
+      {Array.isArray(posts) && posts.length > 0 ? (
+        posts.map((p) => (
+          <Post post={p} key={p.id} />
+        ))
+      ) : (
+        <h1>No posts available</h1>
+      )}
     </div>
   );
 }
