4037d3ce92f896f2deb48ddb15f18af46f97776d
Rishabh Maloo <76218732+irishabhmaloo@users.noreply.github.com>
2023-11-03 16:08:18 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,7 @@ const path = require("path");
 
 dotenv.config();
 app.use(express.json());
-app.use("/images", express.static(path.join(__dirname, "/images")));
-app.use(cors()) 
+app.use("/images", express.static(path.join(__dirname, "/images"))); 
 
 mongoose
   .connect(process.env.MONGO_URL, {
